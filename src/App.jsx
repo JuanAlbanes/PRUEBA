ccc7b9eadaa5ed5bb9e10366a56f2cf241979d2a
JuanAlbanes <juanialbanescastro@gmail.com>
2025-10-09 11:44:21 -0300

    actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/* import React from 'react'
 import { Route, Routes } from 'react-router'
 import RegisterScreen from './Screens/RegisterScreen/RegisterScreen'
 import { LoginScreen } from './Screens/LoginScreen/LoginScreen'
@@ -9,6 +9,29 @@ function App() {
   return (
 
     <Routes>
+      <Route path='/login' element={<LoginScreen/>} />
+      <Route path='/register' element={<RegisterScreen/>} />
+      <Route element={<AuthMiddleware/>}>
+        <Route path='/home' element={<h1>Home</h1>}/>
+      </Route>
+    </Routes>
+
+  )
+}
+
+export default App */
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import RegisterScreen from './Screens/RegisterScreen/RegisterScreen'
+import { LoginScreen } from './Screens/LoginScreen/LoginScreen'
+import AuthMiddleware from './Middlewares/AuthMiddleware'
+
+function App() {
+
+  return (
+
+    <Routes>
+      <Route path='/' element={<LoginScreen/>} />
       <Route path='/login' element={<LoginScreen/>} />
       <Route path='/register' element={<RegisterScreen/>} />
       <Route element={<AuthMiddleware/>}>
